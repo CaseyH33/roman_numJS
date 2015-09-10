896a950bff0bb7df105a79a0a7a637d89db0ee2d
Sam Martinez & Casey Heitz <student@epicodus.com>
2015-09-10 13:49:36 -0700

    homepage is made for. works

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,31 @@ var romanize = function(num) {
     var number_list = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     var output = "";
 
-    for (var i=0; i<= roman_numerals.length; i++) {
-        while(num >= number_list[i]) {
-            output+= roman_numerals[i];
-            num -= number_list[i];
+    if(num <= 0 || num >= 4000) {
+        output = "invalid number";
+    } else {
+
+        for (var i = 0; i <= roman_numerals.length; i++) {
+            while(num >= number_list[i]) {
+                output += roman_numerals[i];
+                num -= number_list[i];
+            }
         }
     }
     return output;
 }
-//
-//
-//
-// $(document).ready(function() {
-//
-//     $("form#countUp").submit(function(event) {
-//         var num = parseInt($("input#num").val());
-//         var count = parseInt($("input#count").val());
-//
-//         var result = countUp(num, count);
-//
-//         $(".output").text(result.join(", "));
-//         $("#result").show();
-//         event.preventDefault();
-//     });
-// });
+
+
+
+$(document).ready(function() {
+
+    $("form#Roman").submit(function(event) {
+        var rome = parseInt($("input#rome").val());
+
+        var result = romanize(rome);
+
+        $(".output").text(result);
+        $("#result").show();
+        event.preventDefault();
+    });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -12,4 +12,12 @@ describe('romanize', function() {
     it("returns MMXV if 2015 is entered", function(){
         expect(romanize(2015)).to.eql("MMXV");
     });
+
+    it("returns error if 0 is entered", function(){
+        expect(romanize(0)).to.eql("invalid number");
+    });
+
+    it("returns error if 5000 is entered", function(){
+        expect(romanize(5000)).to.eql("invalid number");
+    });
 });
